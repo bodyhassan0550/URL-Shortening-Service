afa5127f8498b0eb4f485a2957897c7ae4b4c7ad
bodyhassan0550 <168114870+bodyhassan0550@users.noreply.github.com>
2024-09-08 02:28:48 +0300

    add function update

--- controller/Url.js
+++ controller/Url.js
@@ -1,3 +1,4 @@
+const e = require("express");
 const Url = require("../model/Url");
 
 exports.postAddUrl = (req, res, next) => {
@@ -43,15 +44,80 @@ exports.geturl = (req, res, next) => {
     { shorturl: code },
     { $inc: { count: 1 } }, // Increment the count field by 1
     { new: true }
-  ).then((existurl) => {
-    if (!existurl) {
+  )
+    .then((existurl) => {
+      if (!existurl) {
+        return res.status(404).json({
+          massage: "NOT FOUND",
+        });
+      }
+      return res.status(200).json({
+        url: existurl.url,
+        Short: existurl.shorturl,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).json({
+        message: "Error get the URL",
+        error: err,
+      });
+    });
+};
+exports.geturlstats = (req, res, next) => {
+  const code = req.params.code;
+  Url.findOne({ shorturl: code })
+    .then((existurl) => {
+      if (!existurl) {
+        return res.status(404).json({
+          massage: "NOT FOUND",
+        });
+      }
+      return res.status(200).json({
+        url: existurl.url,
+        Short: existurl.shorturl,
+        count: existurl.count,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).json({
+        message: "Error get the URL",
+        error: err,
+      });
+    });
+};
+exports.updateUrl = (req, res, next) => {
+  const code = req.params.code;
+  const newUrl = req.body.url;
+
+  Url.findOne({ shorturl: code }).then((foundurl) => {
+    if (!foundurl) {
       return res.status(404).json({
         massage: "NOT FOUND",
       });
     }
-    return res.status(200).json({
-      url: existurl.url,
-      Short : existurl.shorturl
+    Url.findOne({ url: newUrl }).then((existurl) => {
+      if (existurl) {
+        return res.status(404).json({
+          massage: "this Url is Already exist",
+          code: existurl.shorturl,
+        });
+      }
+      foundurl.url = newUrl;
+      foundurl.save().then(url=>{
+        res.status(200).json({
+          massage: "successful Update",
+          url: url.url,
+          code: url.shorturl
+        })
+      }).catch((err) => {
+      console.log(err);
+      return res.status(500).json({
+        message: "Error update the URL",
+        error: err,
+      });
+    });
     });
   }).catch((err) => {
       console.log(err);
--- router/Url.js
+++ router/Url.js
@@ -4,5 +4,7 @@ const UrlControl = require("../controller/Url");
 
 router.post("/shorten", UrlControl.postAddUrl);
 router.get("/shorten/:code", UrlControl.geturl);
+router.get("/shorten/:code/stats", UrlControl.geturlstats);
+router.put("/shorten/:code", UrlControl.updateUrl);
 
 module.exports = router;
